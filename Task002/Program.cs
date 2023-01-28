2c228d6bf2b3a0b0017b7748534108477510ac28
Magomedali Gajiev <mag198421@gmail.com>
2023-01-28 08:48:30 +0300

    add some changes Task002

--- Task002/Program.cs
+++ Task002/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.Clear();
+Console.Write("Введите число a: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число b: ");
+int b = int.Parse(Console.ReadLine()!);
+int max; //Максимальное число
+
+if (a > b)
+{
+    max = a;
+}
+else
+{
+    max = b;
+}
+
+Console.WriteLine($"max = {max}");
