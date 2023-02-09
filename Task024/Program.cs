416697d206146d96c74ec4b92cbfb406bd55c5df
Magomedali Gajiev <mag198421@gmail.com>
2023-02-09 17:19:37 +0300

    add Task 024

---
+++ Task024/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+//  7 -> 28 
+//  4 -> 10 
+//  8 -> 36
+
+Console.Clear();
+
+int GetSum(int  number)
+{
+    int sum = 0;
+
+    for (int i = 1; i <= number; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+
+Console.Write("Введите числа A: ");
+int A = int.Parse(Console.ReadLine()!);
+
+System.Console.WriteLine(GetSum(A));
