9713faf8c993cbba50f8b7bd8ea499c69b644989
Magomedali Gajiev <mag198421@gmail.com>
2023-02-09 17:56:22 +0300

    add Task 030

---
+++ Task030/Program.cs
@@ -0,0 +1,28 @@
+﻿// Решение в группах задач: Задача 30: Напишите программу, которая выводит массив из 8 элементов,
+//  заполненный нулями и единицами в случайном порядке. [1,0,1,1,0,1,0,0]
+
+Console.Clear();
+
+int[] GetArray()
+{
+    int[] arr = new int[8];
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(0, 2);
+    }
+    return arr;
+}
+
+void PrintArray(int[] collection)
+{
+    for (int i = 0; i < collection.Length - 1; i++)
+    {
+        Console.Write(collection[i]);
+        if (i != collection.Length)
+        Console.Write(",");
+    }
+}
+
+int[] array = GetArray();
+PrintArray(array);
