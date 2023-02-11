99c0b438441e2bc9bd7bbee300a7c3c2c39c8c0b
Magomedali Gajiev <mag198421@gmail.com>
2023-02-11 17:47:10 +0300

    add Task 034

---
+++ Task034/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите
+// программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+Console.Clear();
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] array = new int[size];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+int CountEvenNumbers(int[] collection)
+{
+    int result = 0;
+    foreach (int item in collection)
+    {
+        result += item % 2 == 0 ? 1 : 0;
+    }
+    return result;
+}
+
+int[] myArray = GetArray(new Random().Next(3, 13), 100, 1000); // Задаем массив размерностью от 3 до 12 с трехзначными числами
+int evenNumbers = CountEvenNumbers(myArray);
+Console.WriteLine($"[{String.Join(", ", myArray)}] -> {evenNumbers}");
