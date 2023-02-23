91f43f408533f37ccb59deff186ba7ed543256df
Magomedali Gajiev <mag198421@gmail.com>
2023-02-23 11:17:39 +0300

    add Task 055

---
+++ Task055/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 55: Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. В случае, если это невозможно,
+// программа должна вывести сообщение для пользователя.
+
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i, j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] Transporant(int[,] array2D)
+{
+    int[,] result = new int[array2D.GetLength(1), array2D.GetLength(0)];
+    for (int i = 0; i < array2D.GetLength(0); i++)
+    {
+        for (int j = 0; j < array2D.GetLength(1); j++)
+        {
+            result[j, i] = array2D[i, j];
+        }
+    }
+    return result;
+}
+
+Console.Clear();
+
+int[,] myArray = GetArray(5, 3, -10, 10);
+PrintArray(myArray);
+Console.WriteLine();
+int[,] newArray = Transporant(myArray);
+PrintArray(newArray);
