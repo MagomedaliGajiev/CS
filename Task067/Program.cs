83b2d793e2fc0bfa92d2216ae34398e44961ab7a
Magomedali Gajiev <mag198421@gmail.com>
2023-02-25 17:09:14 +0300

    add Task 067

---
+++ Task067/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+int GetSumOfDigits(int number)
+{
+    int sumOfDigits = 0;
+    if (number == 0) return sumOfDigits;
+    return sumOfDigits += number % 10 + GetSumOfDigits(number / 10);
+}
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"{num} = {GetSumOfDigits(num)}");
